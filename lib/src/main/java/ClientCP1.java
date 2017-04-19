a518e04a555409f699d8fd75935b86ddb450ad67
pappar1027 <zou_yun@mymail.sutd.edu.sg>
2017-04-19 18:22:43 +0800

    authentication process

--- lib/src/main/java/ClientCP1.java
+++ lib/src/main/java/ClientCP1.java
@@ -2,6 +2,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -15,65 +16,92 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PublicKey;
+import java.security.SecureRandom;
 import java.security.SignatureException;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.util.Arrays;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class ClientCP1 {
-    public static void main(String[] args) throws IOException, CertificateException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    public static void main(String[] args) throws IOException, CertificateException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, SignatureException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
         int portNumber = 4321;
         String hostname="localhost";
         Socket clientSocket = new Socket();
         SocketAddress sockaddr = new InetSocketAddress(hostname, portNumber);
         clientSocket.connect(sockaddr, 100);
         System.out.println("connected");
-        PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);
+        DataOutputStream dOut = new DataOutputStream(clientSocket.getOutputStream());
         DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
 
+        //generate nonce to send
+        SecureRandom random = new SecureRandom();
+        byte nonce[] = new byte[8];
+        random.nextBytes(nonce);
+        //send nonce
+        dOut.writeInt(nonce.length);
+        dOut.write(nonce);
 
-        String message="Hello from the other side";
+        //receive encrypted nonce
+        int EncryptedNonceLength=dIn.readInt();
+        byte[] EncryptedNonce = new byte[EncryptedNonceLength];
+        dIn.readFully(EncryptedNonce, 0, EncryptedNonce.length);
+        System.out.println("encrypted nonce received");
+
+        //ask for cert
+        String message="Send your signed certificate";
+        byte[] messageBytes = message.getBytes();
+        dOut.writeInt(messageBytes.length);
+        dOut.write(messageBytes);
 
 
-//get CA's public key
+
+
+//
+        //get CA's public key
         InputStream fis = new FileInputStream("/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/CA.crt");
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
         X509Certificate CAcert =(X509Certificate)cf.generateCertificate(fis);
         PublicKey CAkey = CAcert.getPublicKey();
 
-        String serverCertName="/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/1001831.crt";
 
-
-
-        //InputStream fisServer = new FileInputStream(serverCertName);
-
+        //// read the cert
         int certLength=dIn.readInt();
-        if(certLength>0) {
-            byte[] certdata = new byte[certLength];
-            dIn.readFully(certdata, 0, certdata.length); // read the message
-            InputStream fisServer = new ByteArrayInputStream(certdata);//may be problematic
-            CertificateFactory cfServer = CertificateFactory.getInstance("X.509");
-            X509Certificate ServerCert =(X509Certificate)cfServer.generateCertificate(fisServer);
-            PublicKey ServerPublicKey = ServerCert.getPublicKey();
-            //verify Server certicate
-            ServerCert.checkValidity();
-            ServerCert.verify(CAkey);
-        }
+        byte[] certdata = new byte[certLength];
+        dIn.readFully(certdata, 0, certdata.length);
+
+        InputStream fisServer = new ByteArrayInputStream(certdata);
+        CertificateFactory cfServer = CertificateFactory.getInstance("X.509");
+        X509Certificate ServerCert =(X509Certificate)cfServer.generateCertificate(fisServer);
+        PublicKey ServerPublicKey = ServerCert.getPublicKey();
+
+        //verify Server certicate
+        ServerCert.checkValidity();
+        ServerCert.verify(CAkey);
         System.out.println("cert verified");
 
+        //verify nonce value
+        Cipher decryptcipher=Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        decryptcipher.init(Cipher.DECRYPT_MODE,ServerPublicKey);
+
+
+        byte[] decryptedNonce=decryptcipher.doFinal(EncryptedNonce);
+
+        //if does not match
+        if(!Arrays.equals(nonce,EncryptedNonce)){
+            clientSocket.close();
+        }
 
 
 
-//        InputStream fisServer = new ByteArrayInputStream(certdata);//may be problematic
-//        CertificateFactory cfServer = CertificateFactory.getInstance("X.509");
-//        X509Certificate ServerCert =(X509Certificate)cfServer.generateCertificate(fisServer);
-//        PublicKey ServerPublicKey = ServerCert.getPublicKey();
-//        //verify Server certicate
-//        ServerCert.checkValidity();
-//        ServerCert.verify(CAkey);
+
+
 
     }
 }
--- lib/src/main/java/ServerCP1.java
+++ lib/src/main/java/ServerCP1.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -9,35 +10,118 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 
 public class ServerCP1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, SignatureException, BadPaddingException, IllegalBlockSizeException {
         ServerSocket serverSocket = new ServerSocket(4321);
         System.out.println("(... expecting connection ...)");
         Socket clientSocket = serverSocket.accept();
-
+        DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
         DataOutputStream dOut = new DataOutputStream(clientSocket.getOutputStream());
+        //read cert
+        File certfile = new File("/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/1001827.crt");
+        FileInputStream fis = new FileInputStream(certfile);
+        byte[] certdata = new byte[(int) certfile.length()];
+        fis.read(certdata);
 
-        File file = new File("/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/1001831.crt");
-        FileInputStream fis = new FileInputStream(file);
-        byte[] certdata = new byte[(int) file.length()];
-            fis.read(certdata);
+        File pubKeyFile =new File("/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/publicServer.der");
+        File privKeyFile =new File("/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/privateServer.der");
 
 
-//        System.out.println(new String(data));
-        //send cert over in bytes
-        dOut.writeInt(certdata.length);
-        dOut.write(certdata);
-        fis.close();
-        System.out.println("cert sent");
+        // read public key DER file
+        DataInputStream dis = new DataInputStream(new FileInputStream(pubKeyFile));
+        byte[] pubKeyBytes = new byte[(int)pubKeyFile.length()];
+        dis.readFully(pubKeyBytes);
 
-        //BufferedReader serverCert=new BufferedReader(new FileReader("/Users/zouyun/Desktop/academic-stuff/ProgrammingAssignment2/lib/src/main/java/1001831.crt"));
-        //System.out.println(serverCert.read());
-        //System.out.println(serverCert.read());
+        dis.close();
 
-//        clientSocket.close();
+        // read private key DER file
+        dis = new DataInputStream(new FileInputStream(privKeyFile));
+        byte[] privKeyBytes = new byte[(int)privKeyFile.length()];
+        dis.read(privKeyBytes);
+
+        dis.close();
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");;
+//
+//
+//        // decode public key
+        X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(pubKeyBytes);
+        RSAPublicKey pubKey=(RSAPublicKey) keyFactory.generatePublic(pubSpec);
+//
+
+//        // decode private key
+        PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(privKeyBytes);
+        RSAPrivateKey privKey= (RSAPrivateKey) keyFactory.generatePrivate(privSpec);
+//
+////
+////        //receive nonce
+        int nonceLength=dIn.readInt();
+        byte[] nonce = new byte[nonceLength];
+        dIn.readFully(nonce, 0, nonce.length);
+//
+//        //encrypt nonce with privateKey
+        Cipher cipher=Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE,privKey);
+        byte[] encryptedNonce=cipher.doFinal(nonce);
+
+        //send encrypted nonce
+        dOut.writeInt(encryptedNonce.length);
+        dOut.write(encryptedNonce);
+        System.out.println("encrypted nonce sent");
+
+        //if ask for cert:
+        int MessageLength=dIn.readInt();
+        byte[] messageBytes = new byte[MessageLength];
+        dIn.readFully(messageBytes, 0, messageBytes.length);
+        String message=new String(messageBytes);
+        System.out.println(message);
+        if(message.equals("Send your signed certificate")){
+            //        //send cert over in bytes
+            dOut.writeInt(certdata.length);
+            dOut.write(certdata);
+            fis.close();
+            System.out.println("cert sent");
+
+        }
+
+
+
+
+
+
+
+
+//        Signature signer = Signature.getInstance("SHA1withRSA");
+//        signer.initSign(privKey); // PKCS#8 is preferred
+//        signer.update(nonce);
+//        byte[] signature = signer.sign();
+
+//        byte[] encryptedNonce=cipher.doFinal(nonce);
+
+
+
+
+//
+////        clientSocket.close();
 
 
 
